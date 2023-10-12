fa01da3fb7a6208d1b99486e8007be51615e0659
BOUABID SOUFIANE <bouabid.soufiane1@gmail.com>
2023-10-12 11:00:27 +0100

    Add update button

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
 
-export default {
   build: {
     rollupOptions: {
       external: ["@fortawesome/free-solid-svg-icons"],
     },
   },
-};
+})
 
